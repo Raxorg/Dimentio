11f55aeb1dad0c2491bad67fa2eba132b9e56811
Groxar <lrdolucho@gmail.com>
2026-03-14 04:59:38 -0400

    ModelProperties upgraded

--- core/src/main/java/com/epicness/fundamentals/stuff/shapes/tridimensional/plane/PlaneProperties.java
+++ core/src/main/java/com/epicness/fundamentals/stuff/shapes/tridimensional/plane/PlaneProperties.java
@@ -1,32 +1,24 @@
 package com.epicness.fundamentals.stuff.shapes.tridimensional.plane;
 
 import static com.epicness.fundamentals.constants.Constants3D.LIGHTLESS_TEXTURED_ATTRIBUTES;
-import static com.epicness.fundamentals.constants.Constants3D.MATERIAL_ID;
 
-import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.g3d.Material;
-import com.badlogic.gdx.graphics.g3d.attributes.BlendingAttribute;
-import com.badlogic.gdx.graphics.g3d.attributes.FloatAttribute;
-import com.badlogic.gdx.graphics.g3d.attributes.IntAttribute;
 import com.epicness.fundamentals.stuff.shapes.tridimensional.model.ModelProperties;
 
 public class PlaneProperties extends ModelProperties {
 
     public final float width, height;
-    public final Material material;
-    public final long attributes;
 
     public PlaneProperties(float width, float height, Material material, long attributes) {
+        super(material, attributes);
         this.width = width;
         this.height = height;
-        this.material = material;
-        this.attributes = attributes;
     }
 
     public PlaneProperties(float width, float height) {
         this(
             width, height,
-            new Material(MATERIAL_ID, new BlendingAttribute(), FloatAttribute.createAlphaTest(0f), IntAttribute.createCullFace(GL20.GL_NONE)),
+            createDefaultMaterial(),
             LIGHTLESS_TEXTURED_ATTRIBUTES
         );
     }
